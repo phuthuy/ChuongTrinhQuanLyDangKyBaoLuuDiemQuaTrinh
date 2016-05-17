655231f9b79bb280eedb6114fd85c0be32f6e802
phuthuy <nguyenthuy35nb@gmail.com>
2016-05-18 01:46:42 +0700

    tktheonguoithu

---
+++ frm_tktheonguoithu.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ThuHocPhi.Views.Thuhocphi.TKHP_theonguoithu
+{
+    partial class frm_tktheonguoithu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.dkbldqtBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dsDongHP = new ThuHocPhi.dsDongHP();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dkbldqtTableAdapter = new ThuHocPhi.dsDongHPTableAdapters.dkbldqtTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dkbldqtBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsDongHP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dkbldqtBindingSource
+            // 
+            this.dkbldqtBindingSource.DataMember = "dkbldqt";
+            this.dkbldqtBindingSource.DataSource = this.dsDongHP;
+            // 
+            // dsDongHP
+            // 
+            this.dsDongHP.DataSetName = "dsDongHP";
+            this.dsDongHP.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            reportDataSource1.Name = "Datadstheonguoithu";
+            reportDataSource1.Value = this.dkbldqtBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "ThuHocPhi.Views.Thuhocphi.TKHP_theonguoithu.rp_dstheonguoithu.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(711, 365);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // dkbldqtTableAdapter
+            // 
+            this.dkbldqtTableAdapter.ClearBeforeFill = true;
+            // 
+            // frm_tktheonguoithu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 365);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frm_tktheonguoithu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_tktheonguoithu";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frm_tktheonguoithu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dkbldqtBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsDongHP)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource dkbldqtBindingSource;
+        private dsDongHP dsDongHP;
+        private dsDongHPTableAdapters.dkbldqtTableAdapter dkbldqtTableAdapter;
+    }
+}
---
+++ frm_tktheonguoithu.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThuHocPhi.Shares;
+
+namespace ThuHocPhi.Views.Thuhocphi.TKHP_theonguoithu
+{
+    public partial class frm_tktheonguoithu : Form
+    {
+        public frm_tktheonguoithu()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_tktheonguoithu_Load(object sender, EventArgs e)
+        {
+            List<ReportParameter> Parameters = new List<ReportParameter>();
+            //Tạo danh sách các Parameter để truyền cho Report 
+            ReportParameter param = default(ReportParameter);
+            param = new ReportParameter("user", Constants.luutk);
+            //Parameters.Add(param);
+            //param = new ReportParameter("txt_ten", Constants.tensv);
+            //Parameters.Add(param);
+            //param = new ReportParameter("txt_tien", Constants.sotien);
+            Parameters.Add(param);
+            reportViewer1.LocalReport.SetParameters(Parameters);
+            this.dkbldqtTableAdapter.FillBy(this.dsDongHP.dkbldqt, Constants.luuhk, Constants.luutk);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
